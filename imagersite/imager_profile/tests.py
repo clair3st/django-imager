42b92692f0da7801543953613fe6ad8f3a2f7a06
Colin Lamont <colinlamont@gmail.com>
2017-01-16 19:37:43 -0800

    added some tests for users and profiles.

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,12 +1,12 @@
 """Tests for the imager site app."""
 from django.test import TestCase
 from django.contrib.auth.models import User
-from lender_profile.models import PatronProfile
+from imager_profile.models import UserProfile
 import factory
 
 
-class ProfileTestCase(TestCase):
-    """The Profile Model test runner."""
+class UserTestCase(TestCase):
+    """The User Model test class."""
 
     class UserFactory(factory.django.DjangoModelFactory):
         class Meta:
@@ -19,26 +19,25 @@ class ProfileTestCase(TestCase):
 
     def setUp(self):
         """The appropriate setup for the appropriate test."""
-        self.foo = "bar"
         self.users = [self.UserFactory.create() for i in range(20)]
 
-    def thing_and_stuff(self):
-        self.thing = "stuff"
-
     def test_profile_is_made_when_user_is_saved(self):
-        """."""
-        self.thing_and_stuff()
-        self.assertTrue(PatronProfile.objects.count() == 20)
-        self.assertTrue(self.thing == "stuff")
+        """Test user profile is created at some time as user."""
+        self.assertTrue(UserProfile.objects.count() == 20)
 
     def test_profile_is_associated_with_actual_users(self):
-        """."""
-        profile = PatronProfile.objects.first()
+        """Test user profile is affiliated with a user instance."""
+        profile = UserProfile.objects.first()
         self.assertTrue(hasattr(profile, "user"))
         self.assertIsInstance(profile.user, User)
 
     def test_user_has_profile_attached(self):
-        """."""
+        """Test the user insance has a profile attached."""
         user = self.users[0]
         self.assertTrue(hasattr(user, "profile"))
-        self.assertIsInstance(user.profile, PatronProfile)
+        self.assertIsInstance(user.profile, UserProfile)
+
+    def test_active_user_method(self):
+        """Test we can detect active users."""
+        active_users = UserProfile.active
+        self.assertTrue(active_users.count() == 20)
