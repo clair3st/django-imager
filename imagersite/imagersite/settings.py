9c24d05bbb11272ac22abf29e42cae17a1be1db0
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-16 17:07:37 -0800

    added databse information

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'imager_profile'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'imagersite',
+        'USER': os.environ["USER_NAME"],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'test_imager'
+        }
     }
 }
 
