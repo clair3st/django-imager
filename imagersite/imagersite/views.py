e53c4667c3582ed710be498b54260f6085d084ae
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-17 17:54:21 -0800

    updated route in views

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,11 +1,11 @@
+"""View for website."""
+
 from django.shortcuts import render
 
 def home_view(request):
     """Home view callable, for the home page."""
-    template = loader.get_template("imagersite/home.html")
-    response = template.render()
-    return render(request, "templates/home.html", {"variable": "value"})
+    return render(request, "imagersite/home.html")
 
 
-def test_view(request, num=None, word=None):
-        return render(request, "templates/")
+# def test_view(request, num=None, word=None):
+#         return render(request, "templates/")
