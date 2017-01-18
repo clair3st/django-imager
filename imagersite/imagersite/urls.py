d642689f27dc4d783d67835803dbc85effb23275
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-17 20:02:02 -0800

    added registration urls

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,17 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from imagersite.views import home_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^$', home_view, name="home"),
     # url(r'^(?P<num>/(?P<word>$', test_view, name="test"),
     url(r'^login$', auth_views.login, name="login"),
     url(r'^logout$', auth_views.logout, name="logout"),
-    url(r'^registration$', include('registration.backends.model_activation.urls'), name="registration"),
 ]
-
