079867a7cf0063ae0094315d1f37373df9706401
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-18 20:42:44 -0800

    added models to admin

--- imagersite/imager_images/admin.py
+++ imagersite/imager_images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from imager_images.models import Album, Photo
+
 
 # Register your models here.
+class AlbumAdmin(admin.ModelAdmin):
+    """Display list for admin."""
+
+    list_display = ("title", "description", "date_uploaded")
+
+admin.site.register(Album, Photo)
