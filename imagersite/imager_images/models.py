c3582d41f9337b41e5a7c664008266e312d95415
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-18 19:53:22 -0800

    created a photo model

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Photo(models.Model):
+    """One individaul photo."""
+
+    image_file = models.ImageField(upload_to="")
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    date_uploaded = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(blank=True, null=True)
+    date_published = models.DateField(blank=True, null=True)
+
+
+class Album(models.Model):
+    """Album class, one user, many photos."""
+
+    pass
