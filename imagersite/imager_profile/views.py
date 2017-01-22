f61fa285e4ff0c2be63b6972a08bd033a36cc89f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-22 12:44:14 -0800

    added view for profile page

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""Views for imager profile."""
 
-# Create your views here.
+from django.shortcuts import render
+from imager_profile.models import UserProfile
+from django.contrib.auth.models import User
+
+
+# Create your views here
+def profile_view(request, username):
+    """Render profile view."""
+    the_user = User.objects.get(username=username)
+    profile = the_user.profile
+    # photos = profile.photos.all()
+    return render(request, 'imager_profile/profile.html', {'profile': profile})
