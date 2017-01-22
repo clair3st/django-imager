98cf57712f4e3432fed6ba5d3ac4c5b652598cba
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-22 13:37:35 -0800

    added views that need to be defined

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,7 +1,7 @@
 """Views for imager profile."""
 
 from django.shortcuts import render
-from imager_profile.models import UserProfile
+# from imager_profile.models import UserProfile
 from django.contrib.auth.models import User
 
 
@@ -10,5 +10,4 @@ def profile_view(request, username):
     """Render profile view."""
     the_user = User.objects.get(username=username)
     profile = the_user.profile
-    # photos = profile.photos.all()
     return render(request, 'imager_profile/profile.html', {'profile': profile})
