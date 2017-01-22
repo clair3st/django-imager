1c1ef45a04474eabb3c7b672d5781d17555d8948
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-22 13:36:43 -0800

    added routes for images views

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -5,12 +5,6 @@ from imager_profile.views import (
     profile_view
 )
 
-# urlpatterns = [
-#     url(r'^$', profile_view, name="profile")
-# ]
-
 urlpatterns = [
-    url(r'^(?P<username>\w+)/$',
-        profile_view,
-        name='profile'),
+    url(r'^(?P<username>\w+)/$', profile_view, name='profile')
 ]
