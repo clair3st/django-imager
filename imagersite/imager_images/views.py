3434aa65e0f616f31372fa28395ecae760f26211
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-22 13:48:24 -0800

    adding basic image views.

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def library_view(request):
+    """Show a users galleries and photos."""
+    pass
+
+
+def album_list(request):
+    """Show a list of all public albums."""
+    pass
+
+
+def photo_list(request):
+    """Show a list of all public photos."""
+    pass
+
+
+def photo_detail(request):
+    """Show detail view of a photo."""
+    pass
+
+
+def album_detail(request):
+    """Show a list of all photos in an album."""
+    pass
