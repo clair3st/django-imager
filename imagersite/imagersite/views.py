74d0382c0363256c88032dab8cbec86899e8a94a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-23 10:36:28 -0800

    tidied up home page route for random image

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,21 +1,16 @@
 """View for website."""
 from imager_images.models import Photo
 from django.shortcuts import render
-from random import randint
 
 
 def home_view(request):
     """Home view callable, for the home page."""
+    import random
     all_imgs = Photo.objects.filter(published="PUBLIC")
-    img_len = len(all_imgs)
 
     try:
-        img_num = randint(1, img_len) - 1
-    except ValueError:
-        img_num = 0
-
-    try:
-        random_img = all_imgs[img_num]
+        random_img = random.choice(all_imgs)
     except IndexError:
         random_img = None
+
     return render(request, "imagersite/home.html", {'photo': random_img})
