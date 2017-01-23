6fd76923356712125179f0def286801869adecf5
Colin Lamont <colinlamont@gmail.com>
2017-01-22 17:27:18 -0800

    radom img on home page.

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,10 +1,17 @@
 """View for website."""
-
+from imager_images.models import Photo
 from django.shortcuts import render
+from random import randint
+
 
 def home_view(request):
     """Home view callable, for the home page."""
-    return render(request, "imagersite/home.html")
-
-# def test_view(request, num=None, word=None):
-#         return render(request, "templates/")
+    import pdb; pdb.set_trace()
+    all_imgs = Photo.objects.filter(published="PUBLIC")
+    img_len = len(all_imgs)
+    img_num = randint(1, img_len)
+    try:
+        random_img = all_imgs[img_num] - 1
+    except IndexError:
+        random_img = all_imgs[7]
+    return render(request, "imagersite/home.html", {'photo': random_img})
