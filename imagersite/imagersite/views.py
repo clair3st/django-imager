a067549605b6a22596bf1f96e36f263a6550a3a0
Colin Lamont <colinlamont@gmail.com>
2017-01-22 17:39:09 -0800

    fixed random img selection from home page.

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -6,12 +6,11 @@ from random import randint
 
 def home_view(request):
     """Home view callable, for the home page."""
-    import pdb; pdb.set_trace()
     all_imgs = Photo.objects.filter(published="PUBLIC")
     img_len = len(all_imgs)
-    img_num = randint(1, img_len)
+    img_num = randint(1, img_len) - 1
     try:
-        random_img = all_imgs[img_num] - 1
+        random_img = all_imgs[img_num]
     except IndexError:
         random_img = all_imgs[7]
     return render(request, "imagersite/home.html", {'photo': random_img})
