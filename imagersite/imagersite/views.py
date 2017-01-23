d0dc15a38ca2aefd9700c3ed25ea11aadddc99ff
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-22 23:24:45 -0800

    added error handling

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -8,9 +8,14 @@ def home_view(request):
     """Home view callable, for the home page."""
     all_imgs = Photo.objects.filter(published="PUBLIC")
     img_len = len(all_imgs)
-    img_num = randint(1, img_len) - 1
+
+    try:
+        img_num = randint(1, img_len) - 1
+    except ValueError:
+        img_num = 0
+
     try:
         random_img = all_imgs[img_num]
     except IndexError:
-        random_img = all_imgs[7]
+        random_img = None
     return render(request, "imagersite/home.html", {'photo': random_img})
