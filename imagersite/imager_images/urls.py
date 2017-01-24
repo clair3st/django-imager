fb1d52dab66785ca82f1a438c44fdca11a4f401d
Colin Lamont <colinlamont@gmail.com>
2017-01-24 14:24:37 -0800

    changed to class based views for imager imager.

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -4,17 +4,17 @@ from django.conf.urls import url
 
 from imager_images.views import (
     PhotoList,
-    library_view,
+    LibraryView,
     AlbumList,
-    photo_detail,
-    album_detail,
+    PhotoDetail,
+    AlbumDetail,
 )
 
 urlpatterns = [
-    url(r'^library/$', library_view, name='library'),
+    url(r'^library/$', LibraryView.as_view(), name='library'),
     url(r'^photos/$', PhotoList.as_view(), name='photo_list'),
     url(r'^albums/$', AlbumList.as_view(), name='album_list'),
-    url(r'^photos/(?P<pk>\d+)$', photo_detail, name='photo_detail'),
-    url(r'^albums/(?P<pk>\d+)$', album_detail, name='album_detail'),
+    url(r'^photos/(?P<pk>\d+)$', PhotoDetail.as_view(), name='photo_detail'),
+    url(r'^albums/(?P<pk>\d+)$', AlbumDetail.as_view(), name='album_detail'),
 
 ]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,24 +1,21 @@
 """Views for imager_images."""
 
-from django.shortcuts import render
 from imager_images.models import Photo, Album
 from django.views.generic import TemplateView
 
 
-def library_view(request):
-    """Show a users galleries and photos."""
-    the_user = request.user
-    user_photo = Photo.objects.filter(photographer=the_user.profile)
-    user_album = Album.objects.filter(owner=the_user.profile)
-    return render(request,
-                  'imager_images/gallery.html',
-                  {'albums': user_album, 'photos': user_photo})
+class LibraryView(TemplateView):
+    """Class based view for Library."""
 
+    template_name = 'imager_images/gallery.html'
 
-def album_list(request):
-    """Show a list of all public albums."""
-    albums = Album.objects.filter(published="PUBLIC")
-    return render(request, 'imager_images/albums.html', {'albums': albums})
+    def get_context_data(self):
+        """Show a users galleries and photos."""
+        the_user = self.request.user
+        user_photos = Photo.objects.filter(photographer=the_user.profile)
+        user_albums = Album.objects.filter(owner=the_user.profile)
+        context = {'albums': user_albums, 'photos': user_photos}
+        return context
 
 
 class AlbumList(TemplateView):
@@ -45,17 +42,25 @@ class PhotoList(TemplateView):
         return context
 
 
-def photo_detail(request, pk):
-    """Show detail view of a photo."""
-    photo_id = Photo.objects.get(pk=pk)
-    return render(request,
-                  'imager_images/photo_detail.html',
-                  {'photo': photo_id})
+class PhotoDetail(TemplateView):
+    """Class based view for Photo Detail."""
+
+    template_name = "imager_images/photo_detail.html"
+
+    def get_context_data(self, pk):
+        """Show detail view of photo."""
+        photo_id = Photo.objects.get(pk=pk)
+        context = {'photos': photo_id}
+        return context
 
 
-def album_detail(request, pk):
-    """Show a list of all photos in an album."""
-    album_id = Album.objects.get(pk=pk)
-    return render(request,
-                  'imager_images/album_detail.html',
-                  {'album': album_id})
+class AlbumDetail(TemplateView):
+    """Class based view for Album detail."""
+
+    template_name = "imager_images/album_detail.html"
+
+    def get_context_data(self, pk):
+        """Show a list of all photos in an album."""
+        album_id = Album.objects.filter(pk=pk)
+        context = {'photos': album_id}
+        return context
