bc3fafe1f49f6e9f942d8b4305c73a1c565d323b
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-24 14:16:33 -0800

    added class based profile to url.

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,16 +1,12 @@
 """Url patterns for imager profile."""
 
 from django.conf.urls import url
-from django.views.generic import TemplateView
 
 from imager_profile.views import (
-    profile_view
+    ProfileView
 )
 
 urlpatterns = [
-    url(r'^$', profile_view, name='my_profile'),
-
-    url(r'^(?P<username>\w+)/$',
-        TemplateView.as_view(template_name="imager_profile/profile.html"),
-        name='profile')
+    url(r'^$', ProfileView.as_view(), name='my_profile'),
+    url(r'^(?P<username>\w+)/$', ProfileView.as_view(), name='profile')
 ]
