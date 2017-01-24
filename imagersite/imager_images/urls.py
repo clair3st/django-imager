dba9a07acfc374a94a3e677f067b4b697aa7f487
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-24 13:13:11 -0800

    trying to get class based views

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,6 +1,9 @@
 """Urls for images/ routes."""
 
 from django.conf.urls import url
+from django.views.generic import TemplateView
+from imager_images.models import Photo
+
 from imager_images.views import (
     library_view,
     album_list,
@@ -11,7 +14,10 @@ from imager_images.views import (
 
 urlpatterns = [
     url(r'^library/$', library_view, name='library'),
-    url(r'^photos/$', photo_list, name='photo_list'),
+    url(r'^photos/$',
+        TemplateView.as_view(template_name="imager_images/photos.html"),
+        name='photo_list',
+        ),
     url(r'^albums/$', album_list, name='album_list'),
     url(r'^photos/(?P<pk>\d+)$', photo_detail, name='photo_detail'),
     url(r'^albums/(?P<pk>\d+)$', album_detail, name='album_detail'),
