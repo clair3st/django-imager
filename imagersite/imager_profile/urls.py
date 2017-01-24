3f6aaf91ad588e73067cada09e42cc3d7537615c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-24 14:00:49 -0800

    trying to make profile work

--- imagersite/imager_profile/urls.py
+++ imagersite/imager_profile/urls.py
@@ -1,6 +1,8 @@
 """Url patterns for imager profile."""
 
 from django.conf.urls import url
+from django.views.generic import TemplateView
+
 from imager_profile.views import (
     profile_view
 )
@@ -8,5 +10,7 @@ from imager_profile.views import (
 urlpatterns = [
     url(r'^$', profile_view, name='my_profile'),
 
-    url(r'^(?P<username>\w+)/$', profile_view, name='profile')
+    url(r'^(?P<username>\w+)/$',
+        TemplateView.as_view(template_name="imager_profile/profile.html"),
+        name='profile')
 ]
