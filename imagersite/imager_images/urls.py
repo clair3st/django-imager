5a94c93ca0e076832c14dfcb4dd5f053d8605727
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-24 14:00:00 -0800

    added url with class based view

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,24 +1,19 @@
 """Urls for images/ routes."""
 
 from django.conf.urls import url
-from django.views.generic import TemplateView
-from imager_images.models import Photo
 
 from imager_images.views import (
+    PhotoList,
     library_view,
-    album_list,
-    photo_list,
+    AlbumList,
     photo_detail,
     album_detail,
 )
 
 urlpatterns = [
     url(r'^library/$', library_view, name='library'),
-    url(r'^photos/$',
-        TemplateView.as_view(template_name="imager_images/photos.html"),
-        name='photo_list',
-        ),
-    url(r'^albums/$', album_list, name='album_list'),
+    url(r'^photos/$', PhotoList.as_view(), name='photo_list'),
+    url(r'^albums/$', AlbumList.as_view(), name='album_list'),
     url(r'^photos/(?P<pk>\d+)$', photo_detail, name='photo_detail'),
     url(r'^albums/(?P<pk>\d+)$', album_detail, name='album_detail'),
 
