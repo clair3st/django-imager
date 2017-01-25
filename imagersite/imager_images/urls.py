af1bc98053089946b43bd2346b7d2de6462b4846
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-24 16:11:32 -0800

    added urls for add

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -16,5 +16,7 @@ urlpatterns = [
     url(r'^albums/$', AlbumList.as_view(), name='album_list'),
     url(r'^photos/(?P<pk>\d+)$', PhotoDetail.as_view(), name='photo_detail'),
     url(r'^albums/(?P<pk>\d+)$', AlbumDetail.as_view(), name='album_detail'),
+    url(r'^albums/add/$', AlbumList.as_view(), name='album_add'),
+    url(r'^photos/add/$', AlbumList.as_view(), name='photo_add'),
 
 ]
