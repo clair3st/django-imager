9167abbe9e31b5e5a7dfbcaa43feaef76b77add3
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-25 16:28:10 -0800

    added a default for email pw.

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -142,6 +142,6 @@ EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'coding.projects17@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD", "")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
