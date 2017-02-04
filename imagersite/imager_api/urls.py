e19eaa3389cb3eb8702f33270363797d40aed5d5
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-04 14:01:28 -0800

    added urls for api

---
+++ imagersite/imager_api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from imager_api.views import PhotoViewSet
+
+
+urlpatterns = [
+    url(
+        r'^(?P<version>(v1))$',
+        PhotoViewSet.as_view({'get': 'list'}),
+        name='api_photo',
+    ),
+]
