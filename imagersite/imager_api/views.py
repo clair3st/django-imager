c154122b8a54c7660602ab6a62fea8a1053c700e
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-04 15:54:02 -0800

    refactored view for api

--- imagersite/imager_api/views.py
+++ imagersite/imager_api/views.py
@@ -1,6 +1,6 @@
 """View API endpoint."""
 
-from rest_framework import viewsets, response, permissions
+from rest_framework import viewsets, permissions
 from imager_images.models import Photo
 from imager_api.serializers import PhotoSerializer
 from imager_api.permissions import IsOwnerOrReadOnly
@@ -9,13 +9,10 @@ from imager_api.permissions import IsOwnerOrReadOnly
 class PhotoViewSet(viewsets.ModelViewSet):
     """API endpoint that allows users to be viewed or edited."""
 
-    queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
-    def list(self, request, *args, **kwargs):
-        """Filter photos by user."""
-        queryset = Photo.objects.filter(photographer=request.user.profile)
-        serializer = PhotoSerializer(queryset, many=True)
-        return response.Response(serializer.data)
+    def get_queryset(self):
+        """Get queryset for photographer."""
+        return Photo.objects.filter(photographer=self.request.user.profile)
