6f78f994f144e0f436b67dc23f29a7dda13460c4
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-04 14:00:07 -0800

    added a view for the api

--- imagersite/imager_api/views.py
+++ imagersite/imager_api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""View API endpoint."""
 
-# Create your views here.
+from rest_framework import viewsets, response, permissions
+from imager_images.models import Photo
+from imager_api.serializers import PhotoSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """API endpoint that allows Photos."""
+
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    def photo_list(self, request, *args, **kwargs):
+        """Get photos owned by the user."""
+        queryset = Photo.objects.filter(photographer=request.user.profile)
+        serializer = PhotoSerializer(queryset, many=True)
+        return response.Response(serializer.data)
