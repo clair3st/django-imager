1a322285070332da948dbcbd366e973cc1bf9af5
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-04 15:53:49 -0800

    added name

--- imagersite/imager_api/urls.py
+++ imagersite/imager_api/urls.py
@@ -7,6 +7,6 @@ from imager_api.views import PhotoViewSet
 urlpatterns = [
     url(
         r'^(?P<version>(v1))$',
-        PhotoViewSet.as_view({'get': 'list'})
+        PhotoViewSet.as_view({'get': 'list'}), name='api_list'
     ),
 ]
