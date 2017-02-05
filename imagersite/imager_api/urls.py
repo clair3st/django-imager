4e607e464cfacdd455ad1063f3b3e951ddc17491
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-04 16:10:29 -0800

    fixing url patterns

--- imagersite/imager_api/urls.py
+++ imagersite/imager_api/urls.py
@@ -5,8 +5,5 @@ from imager_api.views import PhotoViewSet
 
 
 urlpatterns = [
-    url(
-        r'^(?P<version>(v1))$',
-        PhotoViewSet.as_view({'get': 'list'}), name='api_list'
-    ),
+    url(r'^photos/$', PhotoViewSet.as_view({'get': 'list'}), name='api_list'),
 ]
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^profile/', include("imager_profile.urls")),
     url(r"^images/", include('imager_images.urls')),
-    url(r'^api/', include('imager_api.urls'))
+    url(r'^api/v1/', include('imager_api.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
