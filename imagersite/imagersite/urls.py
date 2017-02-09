9671c686c27f8a98577ef535f56d0e9673a0d54f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-09 13:07:18 -0800

    added auth urls

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name="login"),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^profile/', include("imager_profile.urls")),
-    url(r"^images/", include('imager_images.urls'))
+    url(r"^images/", include('imager_images.urls')),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
